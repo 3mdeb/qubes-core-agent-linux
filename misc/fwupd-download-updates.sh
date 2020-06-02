56122dd1944984df7c7e9a8a802d5b9b39dbe8c3
Norbert Kamiński <norbert.kaminski@3mdeb.com>
2020-06-02 18:01:54 +0200

    fwupd-download-updates.sh: Add parsing parameters, download and verify metadata
    Signed-off-by: Norbert Kamiński <norbert.kaminski@3mdeb.com>

--- misc/fwupd-download-updates.sh
+++ misc/fwupd-download-updates.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-FWUPD_UPDATES_DIR=/var/lib/fwupd/fwupd-updates
+FWUPD_UPDATES_DIR=/home/user/.cache/fwupd/fwupd-updates
 
 echo "Running fwupd download script..."
 
 CLEAN=
 CHECK_ONLY=
-FW_HARDWARE=
-OPTS=
-EXARGS=
+METADATA=
+SHASUM=
+UPDATE=
+URL=
+
+
 
 while [ -n "$1" ]; do
     case $1 in
@@ -18,14 +21,23 @@ while [ -n "$1" ]; do
         --clean)
             CLEAN=1
             ;;
-        --hardware=*)
-            FW_HARDWARE=${1#--hardware}
+        --metadata)
+            METADATA=1
+            ;;
+        --url=*)
+            UPDATE=1
+            URL=${1#--url=}
+            ;;
+        --sha=*)
+            SHASUM=${1#--sha=}
             ;;
         -*)
-            OPTS="$OPTS $1"
+            echo "Command $1 unknown. exiting..."
+            exit 1
             ;;
         *)
-            EXARGS="$EXARGS $1"
+            echo "Command $1 unknown. exiting..."
+            exit 1
             ;;
     esac
     shift
@@ -37,22 +49,41 @@ if [ ! -d $FWUPD_UPDATES_DIR ]; then
 fi
 
 if [ "$CHECK_ONLY" == "1" ]; then
-    echo "Check only mode"
+    echo "Check only mode."
 fi
 
 if [ "$CLEAN" == "1" ]; then
-    echo "Cleaning downloaded files"
-    rm -f $FWUPD_UPDATES_DIR/*
+    echo "Cleaning cache."
+    rm -f $FWUPD_UPDATES_DIR/metadata
+    rm -f $FWUPD_UPDATES_DIR/updates
 fi
 
-if [ -f $FWUPD_UPDATES_DIR/sample-image.jpg ]; then
-    echo "File already exists."
-else
-    wget -O $FWUPD_UPDATES_DIR/sample-image.jpg \
-        https://cloud.3mdeb.com/index.php/s/29QbbAqWMw8cSP4/preview
+if [ "$METADATA" == "1" ]; then
+    echo "Downloading metadata."
+    wget -P $FWUPD_UPDATES_DIR/metadata \
+        https://cdn.fwupd.org/downloads/firmware.xml.gz
+    wget -P $FWUPD_UPDATES_DIR/metadata \
+        https://cdn.fwupd.org/downloads/firmware.xml.gz.asc
+    gpg --verify firmware.xml.gz.asc firmware.xml.gz.asc
+    if [ ! $? -eq 0 ]; then
+        rm -f $FWUPD_UPDATES_DIR/metadata/*
+        echo "Wrong signature. Exiting..."
+        exit 1
+    fi
 fi
 
-cmd="/usr/lib/qubes/qrexec-client-vm dom0 qubes.ReceiveUpdates"
+if [ "$UPDATE" == "1" ]; then
+    echo "Downloading firmware update $URL"
+    wget -P $FWUPD_UPDATES_DIR/updates $URL
+    wget -P $FWUPD_UPDATES_DIR/updates $URL
+    if [ ! $? -eq 0 ]; then
+        rm -f $FWUPD_UPDATES_DIR/metadata/*
+        echo "Wrong signature. Exiting..."
+        exit 1
+    fi
+fi
+
+cmd="/usr/lib/qubes/qrexec-client-vm dom0 fwupd.ReceiveUpdates"
 qrexec_exit_code=0
 $cmd || { qrexec_exit_code=$? ; true; };
 if [ ! "$qrexec_exit_code" = "0" ]; then
